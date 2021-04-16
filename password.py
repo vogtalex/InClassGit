d97f0534eba2e80c221b1e44148f257fe3eebca0
Alexander Vogt <vogtalex@flip1.engr.oregonstate.edu>
2021-04-15 17:39:36 -0700

    Finished generatePassword function

--- password.py
+++ password.py
@@ -2,7 +2,10 @@ import string
 import random
 
 def generatePassword(num):
-	
+	char = string.ascii_letters
+	randString = ''.join(random.choice(char) for i in range(num))
+	print(randString)
+
 
 print('\nWelcome to the password generator program')
 num = int(input("Enter a number to generate a password of your designated length: "))
