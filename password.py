e7efc33b6804edb7c4babd03b895abbc323c2e61
Alexander Vogt <vogtalex@flip1.engr.oregonstate.edu>
2021-04-15 17:38:11 -0700

    Created function generatePassword and added two libraries

--- password.py
+++ password.py
@@ -1,5 +1,10 @@
+import string
+import random
 
+def generatePassword(num):
+	
 
 print('\nWelcome to the password generator program')
-
 num = int(input("Enter a number to generate a password of your designated length: "))
+
+generatePassword(num)
