7e16e9e9bcacc944c0bca5aa504726029653d499
Alexander Vogt <vogtalex@flip1.engr.oregonstate.edu>
2021-04-15 17:22:57 -0700

    Implemented findDivisors and added printing of divisors array.

--- divisor.py
+++ divisor.py
@@ -1,9 +1,22 @@
-def findDivisors(num):
+def findDivisors(num , arr):
 	x = 1
-
+	i = 0
+	while x <= num:
+		if (num % x == 0):
+			arr.append(x)
+			i = i + 1
+		x = x + 1
 
 print('\nWelcome to the divisior program')
 
 num = int(input("Enter a number to find the divisors of: "))
+arr = []
 
-findDivisors(num)
+findDivisors(num, arr)
+
+x = len(arr)
+i = 0
+
+while i < x:
+	print(arr[i])
+	i = i + 1
